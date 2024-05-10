1862f66e7399ee9608681a9a53994b211d952684
stevenssebuma <sse23007@byui.edu>
2024-05-10 20:37:13 +0300

    Added Content

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -2,9 +2,6 @@ const option = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'}
 
 document.getElementById("current-date").textContent = new Date().toLocaleDateString('en-US', option)
 
-document.querySelector('.hamburger-menu').addEventListener('click', function() {
-    document.querySelector('nav ul').classList.toggle('show');
-});
 const hamButton = document.querySelector("#hamburger");
 const ul = document.querySelector("ul");
 
