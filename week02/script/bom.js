6cdea9f1e5a73d4646be05ae62e542c33c9ec83f
stevenssebuma <sse23007@byui.edu>
2024-05-31 01:59:50 +0300

    Added Content

---
+++ week02/script/bom.js
@@ -0,0 +1,20 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener("click", () => {
+    if (input.value !=='') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton.textContent = "❌";
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+         });
+         input.value = '';
+    }
+    input.focus();
+    });
