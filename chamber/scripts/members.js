a9a67b69007cbceaf80b2b5fe838a7196c0e7fbf
stevenssebuma <sse23007@byui.edu>
2024-06-01 14:34:09 +0300

    Added Content

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,99 +1,71 @@
-const baseURL = "https://stevenssebuma.github.io/wdd230/chamber/";
-const membersURL = "https://stevenssebuma.github.io/wdd230/chamber/data/members.json";
-const docMembers = document.querySelector("#members");
+const url = 'data/members.json';
+const gridDirectory = document.querySelector('#gridDirectory');
 
-async function getMembers() {
-    try {
-        const response = await fetch(membersURL);
-        if (response.ok) {
-            const data = await response.json();
-            // console.log(data);
-            displayMembers(data.members);
-        }
-        else {
-            throw Error(await response.text());
-        }
-    } catch (error) {
-        console.log(error);
-    }
+async function getDirectory() {
+    const response = await fetch(url);
+    const data = await response.json();
+    //console.table(data.members);
+    displayDirectory(data.companies);
 }
 
-const displayMembers = (members) => {
 
-    members.forEach(member => {
 
-        // create document elements for each card
-        const card = document.createElement("section");
-        const business = document.createElement("h3");
-        const address = document.createElement("address");
-        const street = document.createElement("p");
-        const csz = document.createElement("p");
-        const website = document.createElement("a");
-        const phone = document.createElement("a");
-        const email = document.createElement("a");
-        const logo = document.createElement("img");
-        const membership = document.createElement("p");
-        const category = document.createElement("p");
+const displayDirectory = companies => {
+    companies.forEach((company) => {
+        let cardMem = document.createElement('section');
+            cardMem.classList.add('cardMem');
 
-        // set element properties & values
-        card.setAttribute("class", "card");
-        business.textContent = member.business;
-        street.textContent = member.address.street;
-        let compoundCSZ = `${member.address.city}, ${member.address.state} ${member.address.zip}`;
-        csz.textContent = compoundCSZ;
-        phone.setAttribute("href", `tel:${member.phone}`);
-        phone.textContent = member.phone.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
-        email.setAttribute("href", `mailto:${member.email}`);
-        email.textContent = member.email;
-        website.setAttribute("href", member.website);
-        website.textContent = "Go to website";
-        membership.textContent = `Membership: ${member.membership}`;
-        category.textContent = `Category: ${member.category}`;
+        let name = document.createElement('h2');
+        let address = document.createElement('p');
+        let phone = document.createElement('p');
+        let website = document.createElement('a');
+        let membership = document.createElement('p');
+    
+        let portrait = document.createElement('img');
 
-        // define logo image attributes
-        logo.setAttribute("src", member.img);
-        logo.setAttribute("alt", `Logo for ${member.business}`);
-        logo.setAttribute("loading", "lazy");
-        logo.setAttribute("width", "350");
-        logo.setAttribute("height", "450");
+        name.textContent = `${company.name}`;
+        address.textContent = `${company.address}`;
+        phone.textContent = `${company.phone}`;
+        membership.textContent = `Membership: ${company.membership}`;
+        
+        website.textContent = company.websiteURL;
+        website.href = company.websiteURL;
+        website.setAttribute('target', '_blank');
+    
 
-        // append elements to the document
-        address.appendChild(street);
-        address.appendChild(csz);
+        portrait.setAttribute('src', company.image);
+        portrait.setAttribute('alt', `Logo of ${company.name}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
 
-        card.appendChild(business);
-        card.appendChild(document.createElement("br"));
-        card.appendChild(logo);
-        card.appendChild(document.createElement("br"));
-        card.appendChild(address);
-        card.appendChild(document.createElement("br"));
-        card.appendChild(phone);
-        card.appendChild(document.createElement("br"));
-        card.appendChild(website);
-        card.appendChild(document.createElement("br"));
-        card.appendChild(email);
-        card.appendChild(document.createElement("br"));
-        card.appendChild(membership);
-        card.appendChild(document.createElement("br"));
-        card.appendChild(category);
+        
+        cardMem.appendChild(name);
+        cardMem.appendChild(address);
+        cardMem.appendChild(phone);
+        cardMem.appendChild(membership);
+        cardMem.appendChild(website);
+        cardMem.appendChild(portrait);
+        
 
-        docMembers.appendChild(card);
+        gridDirectory.appendChild(cardMem);
 
     });
 }
-getMembers();
 
-// grid vs list functionality
+getDirectory();
 
-const gridButton = document.querySelector("#grid");
-const listButton = document.querySelector("#list");
+const gridButton = document.querySelector("#gridButton");
+const listButton = document.querySelector("#listButton");
+const display = document.querySelector("#gridArticle");
 
 gridButton.addEventListener("click", () => {
-    docMembers.classList.add("grid");
-    docMembers.classList.remove("list");
+    display.classList.add("gridButton");
+    display.classList.remove("listButton");
 });
 
-listButton.addEventListener("click", () => {
-    docMembers.classList.add("list");
-    docMembers.classList.remove("grid");
+listButton.addEventListener("click", () =>{
+    display.classList.add("listButton");
+    display.classList.remove("gridButton");
+
 });
